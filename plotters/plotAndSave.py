a485605ecbca71786d66ea19c12511111760a98a
Joao Palotti <joaopalotti@gmail.com>
2013-08-01 14:27:04 +0200

    Added option to plot CDFs

--- plotters/plotAndSave.py
+++ plotters/plotAndSave.py
@@ -9,18 +9,18 @@ barwidth=0.18
 #plotGraph(barwidth, saveName=None, pp=None, colors=['c','g','b','r'], PATH_TO_DATA="/home/palotti/Dropbox/tuwien/PhD/logs/logAnalysis/plots/", globString="meshDepth*.data", rebaseString="meshDepth(?P<base>\w*", Ylabel='Percentage of Occurences', Xlabel='Mesh Depth', N=None)
 
 #plotQueriesPerSession("qps.pdf", pp)
-plotGraph(barwidth, saveName="qps.pdf", pp=pp, globString="queriesPerSession*.data", rebaseString="queriesPerSession(?P<base>\w*)", Ylabel="Percentage of Sessions", Xlabel="Session Length (queries)", mapType=int, N=10) 
+#plotGraph(barwidth, saveName="qps.pdf", pp=pp, globString="queriesPerSession*.data", rebaseString="queriesPerSession(?P<base>\w*)", Ylabel="Percentage of Sessions", Xlabel="Session Length (queries)", mapType=int, N=10) 
 
 #plotQuerySize("qs.pdf", pp)
-plotGraph(barwidth, saveName="qs.eps", pp=pp, ignoreString="Relative", globString="queriesSize*.data", rebaseString="queriesSize(?P<base>\w*)", Ylabel="Number of queries (%)", Xlabel="Terms per query", mapType=float, N=5, barPlot=True) 
+plotGraph(barwidth, saveName="qs.eps", pp=pp, ignoreString="Relative", globString="queriesSize*.data", rebaseString="queriesSize(?P<base>\w*)", Ylabel="CDF of Number of queries (%)", Xlabel="Terms per query", mapType=float, N=15, plotType="cdf") 
 
 #plotMesh
-plotGraph(barwidth, saveName="md.pdf", pp=pp, globString="meshDepth*.data", rebaseString="meshDepth(?P<base>\w*)", Ylabel="Percentage of Occurences", Xlabel="Mesh Depth", mapType=int, N=10) 
+#plotGraph(barwidth, saveName="md.pdf", pp=pp, globString="meshDepth*.data", rebaseString="meshDepth(?P<base>\w*)", Ylabel="Percentage of Occurences", Xlabel="Mesh Depth", mapType=int, N=10) 
 
 #number of queries by number of users
-plotGraph(barwidth, saveName="qpu.pdf", pp=pp, globString="queriesPerUser*.data", rebaseString="queriesPerUser(?P<base>\w*)", Ylabel="Number Of Users ($\%$)", Xlabel="Number of Queries", mapType=int, N=10) 
+#plotGraph(barwidth, saveName="qpu.pdf", pp=pp, globString="queriesPerUser*.data", rebaseString="queriesPerUser(?P<base>\w*)", Ylabel="Number Of Users ($\%$)", Xlabel="Number of Queries", mapType=int, N=10) 
 
-plotGraph(barwidth, saveName="qpuA.pdf", pp=pp, globString="queriesPerUser*.data", rebaseString="queriesPerUser(?P<base>\w*)", Ylabel="Number Of Users", Xlabel="Number of Queries", mapType=int, N=10, absolute=True) 
+#plotGraph(barwidth, saveName="qpuA.pdf", pp=pp, globString="queriesPerUser*.data", rebaseString="queriesPerUser(?P<base>\w*)", Ylabel="Number Of Users", Xlabel="Number of Queries", mapType=int, N=10, absolute=True) 
 
 
 #plotGraph(saveName="rq.pdf", pp=pp, globString="queryRanking*.data", rebaseString="queryRanking(?P<base>\w*)", Ylabel="Frequency (%)", Xlabel="Rank of the clicked result", mapType=int, N=11) 
